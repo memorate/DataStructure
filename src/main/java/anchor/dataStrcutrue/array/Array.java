e739b76b808f7fef324e44adc1db91b4fb629096
mawen <mawen@meicloud.com>
2021-02-04 21:29:05 +0800

    update Array.java add get() and optimize code

--- src/main/java/anchor/dataStrcutrue/array/Array.java
+++ src/main/java/anchor/dataStrcutrue/array/Array.java
@@ -17,7 +17,7 @@ public class Array<E> {
     private final int maxSize;
 
     /**
-     * 用于存储数据
+     * 使用链式存储来储存数据
      */
     private final Object[] data;
 
@@ -27,35 +27,65 @@ public class Array<E> {
         } else {
             this.maxSize = maxSize;
             this.data = new Object[maxSize];
+            this.size = 0;
         }
     }
 
     /**
      * 在 Array 末尾添加一个元素
+     *
      * @param element  被添加的元素
-     * @return 若 Array 已满，抛出 OutOfMemoryError; 操作成功返回 true
+     * @return 若 Array 已满，抛出 OutOfMemoryError；操作成功返回 true
      */
     public boolean add(E element) {
-        if (size + 1 > maxSize) {
-            throw new OutOfMemoryError("Array can't afford another element anymore!");
-        } else {
-            data[size++] = element;
-            return true;
-        }
+        rangeCheck(size + 1);
+        data[size++] = element;
+        return true;
     }
 
     /**
-     * 新增/修改 指定 Array 中下标所在的值
-     * @param index
-     * @param element
-     * @return
+     * 新增/修改 Array 中指定的元素
+     *
+     * @param index     Array 的下表
+     * @param element   待添加的元素
+     * @return 越界会抛出 IndexOutOfBoundsException；操作成功返回 true
      */
     public boolean set(int index, E element) {
-        if (index < 0 || index > maxSize) {
-            throw new IndexOutOfBoundsException("index can not out of boundary!");
-        } else {
-            data[index] = element;
-            return true;
+        rangeCheck(index);
+        data[index] = element;
+        return true;
+    }
+
+    /**
+     * 获取 Array 尾部的元素
+     *
+     * @return 尾部的元素
+     */
+    public E get() {
+        return element(size - 1);
+    }
+
+    /**
+     * 根据索引来获取 Array 中的指定元素
+     *
+     * @param index   索引
+     * @return 越界会抛出 IndexOutOfBoundsException
+     */
+    public E get(int index) {
+        return element(index);
+    }
+
+    private E element(int index) {
+        rangeCheck(index);
+        return (E) data[index];
+    }
+
+    private void rangeCheck(int index) {
+        if (size == 0 && index == 0) {
+            throw new NullPointerException("Array is empty!");
+        }
+        if (index < 0 || index >= maxSize) {
+            throw new IndexOutOfBoundsException("Index out of boundary!");
         }
     }
 }
