0272e5473db290157a9cf9af4235c480666d31aa
Anchor <sweetsorrow211@gmail.com>
2021-02-14 15:25:38 +0800

    complete ArrayQueue.java and ArrayQueueTest.java

--- src/main/java/anchor/dataStrcutrue/queue/ArrayQueue.java
+++ src/main/java/anchor/dataStrcutrue/queue/ArrayQueue.java
@@ -8,7 +8,7 @@ public class ArrayQueue<E> {
     /**
      * 使用数组来存储元素
      */
-    private Object[] array;
+    private final Object[] array;
 
     /**
      * 队列中现有元素的个数
@@ -18,7 +18,7 @@ public class ArrayQueue<E> {
     /**
      * 队列的容量
      */
-    private int capacity;
+    private final int capacity;
 
     /**
      * 队列中元素在数组中的起始 index
@@ -34,19 +34,60 @@ public class ArrayQueue<E> {
         array = new Object[capacity];
         this.capacity = capacity;
         size = 0;
-        start = -1;
-        end = 0;
+        start = 0;
+        end = -1;
     }
 
     public void add(E element) {
         full();
-        if (size == 0) {
-            array[start] = element;
-            size++;
-            return;
+        array[++end] = element;
+        size++;
+        if (size == capacity) {
+            end = -1;
         }
-        if (end < capacity - 1) {
+    }
 
+    @SuppressWarnings("unchecked")
+    public E pop() {
+        empty();
+        E target = (E) array[start];
+        array[start] = null;
+        start++;
+        if (start == capacity - 1) {
+            start = 0;
+        }
+        size--;
+        return target;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public String display() {
+        int top = start;
+        StringBuilder builder = new StringBuilder("[");
+        for (int i = 0; i < size; i++) {
+            builder.append(array[top++]);
+            if (top == capacity) {
+                top = 0;
+            }
+            if (i == size - 1) {
+                builder.append("]");
+                break;
+            }
+            builder.append(",");
+        }
+        return builder.toString();
+    }
+
+    private void empty() {
+        if (isEmpty()) {
+            throw new IndexOutOfBoundsException("Queue is empty!");
         }
     }
 
---
+++ src/main/test/anchor/dataStrcutrue/queue/ArrayQueueTest.java
@@ -0,0 +1,48 @@
+package anchor.dataStrcutrue.queue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayQueueTest {
+
+    private ArrayQueue<String> queue = new ArrayQueue<>(5);
+
+    @BeforeEach
+    void init() {
+        queue.add("a");
+        queue.add("b");
+        queue.add("c");
+        queue.add("d");
+    }
+
+    @Test
+    void add() {
+        queue.add("e");
+        assertEquals(5, queue.size());
+        assertThrows(IndexOutOfBoundsException.class, () -> queue.add("f"));
+    }
+
+    @Test
+    void pop() {
+        assertEquals(4, queue.size());
+        assertEquals("a", queue.pop());
+        queue.pop();
+        queue.pop();
+        assertEquals("d", queue.pop());
+        assertTrue(queue.isEmpty());
+        assertThrows(IndexOutOfBoundsException.class, () -> queue.pop());
+    }
+
+    @Test
+    void display() {
+        assertEquals("[a,b,c,d]", queue.display());
+        queue.add("e");
+        assertEquals("[a,b,c,d,e]", queue.display());
+        queue.pop();
+        assertEquals("[b,c,d,e]", queue.display());
+        queue.add("f");
+        assertEquals("[b,c,d,e,f]", queue.display());
+    }
+}
